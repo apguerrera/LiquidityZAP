703518655eb869db9cc20cc759adabc9b3c03e0c
Adrian Guerrera <adrianguerrera@gmail.com>
2020-10-21 01:42:40 +0200

    Deployed V1

--- scripts/deploy_UniswapZAP.py
+++ scripts/deploy_UniswapZAP.py
@@ -1,5 +1,4 @@
 from brownie import *
-from .contract_addresses import *
 import time
 
 
@@ -20,12 +19,32 @@ def deploy_uniswap_zap():
 def main():
     if network.show_active() == 'mainnet':
         # replace with your keys
-        accounts.load("dutchswap")
+        accounts.load("liquidityzap")
 
     # Create Uniswap Liquidity Zap
     uniswap_zap = deploy_uniswap_zap()
     
 
 
+
+# brownie run deploy_UniswapZAP.py --network mainnet      
+#                                                                                                                             ─╯
+# Brownie v1.11.0 - Python development framework for Ethereum
+# LiquidityzapProject is the active project.
+
+# Running 'scripts/deploy_UniswapZAP.py::main'...
+# Enter the password to unlock this account:
+# Transaction sent: 0xc219944451d240a7d5f880cf49d9a619baeb6a344a510ce2ba12929d12cd9e23
+#   Gas price: 30.0 gwei   Gas limit: 935427
+# Waiting for confirmation...
+#   UniswapZAP.constructor confirmed - Block: 11096018   Gas used: 935427 (100.00%)
+#   UniswapZAP deployed at: 0x2cbe8406380E784ea1a24aeEDFbf79788E2721AC
+
+# Transaction sent: 0xc89eec30de8ab84fa122d255ae87656163c589927029a6eb066ccf6c35476097
+#   Gas price: 31.0 gwei   Gas limit: 85828
+# Waiting for confirmation...
+#   UniswapZAP.initUniswapZAP confirmed - Block: 11096020   Gas used: 85828 (100.00%)
+
+# UniswapZAP contract deployed at: 0x2cbe8406380E784ea1a24aeEDFbf79788E2721AC
 
 
