3f90991b980a15aadceb8a4413cb7efd40ad4ba9
Adrian Guerrera <adrianguerrera@gmail.com>
2020-10-22 03:25:49 +0200

    Added balance template

--- contracts/BalancerZAP.sol
+++ contracts/BalancerZAP.sol
@@ -1,8 +1,60 @@
 pragma solidity ^0.6.12;
 
-// To be completed
+
+import "../interfaces/IWETH9.sol";
+import "../interfaces/IERC20.sol";
+import "./Utils/SafeMath.sol";
 
 contract BalancerZAP {
 
+    using SafeMath for uint256;
+
+    address public _token;
+    address public _balancerPool;
+    bool private initialized;
+
+    function initBalancerZAP(address token, address balancerPool) public  {
+        require(!initialized);
+        _token = token;
+        _balancerPool = balancerPool;
+        // approve tokens from balancer
+        initialized = true;
+    }
+
+    fallback() external payable {
+        addLiquidityETHOnly(msg.sender);
+    }
+    receive() external payable {
+        addLiquidityETHOnly(msg.sender);
+    }
+
+    function addLiquidityETHOnly(address payable to) public payable {
+        require(to != address(0), "Invalid address");
+
+        uint256 buyAmount = msg.value;
+        require(buyAmount > 0, "Insufficient ETH amount");
+
+        // Swap ETH for tokens        
+        uint tokenAmount = buyAmount;
+
+        _addLiquidity(tokenAmount, to);
+
+    }
+
+    function addLiquidityTokenOnly(uint256 tokenAmount, address payable to) public payable {
+        require(to != address(0), "Invalid address");
+        require(tokenAmount > 0, "Insufficient token amount");
+        _addLiquidity(tokenAmount, to);
+
+    }
+
+    function _addLiquidity(uint256 /*tokenAmount*/, address payable /*to*/) internal {
+        // transfer tokens to balancer pool
+
+        //mint BPTs 
+        
+        //refund dust tokens
+
+    }
 
 }
